7fd1b01919113cb65e0f487dc3d867e75fc17fe0
MatheusBlanco <msallesblanco@gmail.com>
2025-05-20 23:33:41 -0300

    fix: :package: Fixes icon to work better in android monochrome mode

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -36,139 +36,113 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-
-      body: Center(
-        child:
-            <Widget>[
-              MultiBlocProvider(
-                providers: [
-                  BlocProvider(
-                    create: (context) => UserDisplayCubit()..displayUser(),
-                  ),
-                  BlocProvider(create: (context) => ButtonStateCubit()),
-                  BlocProvider(create: (context) => AuthStateCubit()),
-                ],
-                child: MultiBlocProvider(
-                  providers: [
-                    BlocProvider(
-                      create: (context) => UserDisplayCubit()..displayUser(),
-                    ),
-                    BlocProvider(create: (context) => ButtonStateCubit()),
-                    BlocProvider(create: (context) => AuthStateCubit()),
-                  ],
-                  child: Center(
-                    child: BlocBuilder<UserDisplayCubit, UserDisplayState>(
-                      builder: (context, state) {
-                        if (state is UserLoading) {
-                          return const CircularProgressIndicator();
-                        }
-                        if (state is UserLoaded) {
-                          return Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Consumer(
-                                builder:
-                                    (
-                                      context,
-                                      ThemeNotifier themeNotifier,
-                                      child,
-                                    ) => SwitchListTile(
-                                      title: Text("Dark Mode"),
-                                      onChanged: (val) {
-                                        themeNotifier.toggleTheme();
-                                      },
-                                      value:
-                                          themeNotifier.theme == 'dark'
-                                              ? true
-                                              : false,
-                                    ),
-                              ),
-                              Consumer(
-                                builder:
-                                    (
-                                      context,
-                                      ThemeNotifier themeNotifier,
-                                      child,
-                                    ) => FloatingActionButton(
-                                      heroTag: 'appColorSchemeDynamic',
-                                      onPressed: () {
-                                        themeNotifier.toggleColorScheme(
-                                          'dynamic',
-                                        );
-                                      },
-                                      child: Icon(Icons.add),
-                                    ),
-                              ),
-                              Consumer(
-                                builder:
-                                    (
-                                      context,
-                                      ThemeNotifier themeNotifier,
-                                      child,
-                                    ) => FloatingActionButton(
-                                      heroTag: 'appColorSchemeMain',
-                                      onPressed: () {
-                                        themeNotifier.toggleColorScheme('main');
-                                      },
-                                      child: Icon(Icons.add),
-                                    ),
-                              ),
-
-                              const SizedBox(height: 10),
-                              Text(
-                                state.userEntity.email,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 19,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(32),
-                                child: Consumer(
-                                  builder: (
-                                    context,
-                                    ThemeNotifier themeNotifier,
-                                    child,
-                                  ) {
-                                    return BestyButton(
-                                      title: 'Logout',
-                                      onPressed: () {
-                                        themeNotifier.toggleColorScheme('main');
-                                        if (themeNotifier.theme == 'dark') {
-                                          themeNotifier.toggleTheme();
-                                        }
-                                        context
-                                            .read<ButtonStateCubit>()
-                                            .execute(usecase: SignoutUseCase());
-                                        Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                            builder: (context) => SigninPage(),
-                                          ),
-                                        );
-                                      },
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          );
-                        }
-                        if (state is LoadUserFailure) {
-                          return Text(state.errorMessage);
-                        }
-                        return Container();
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              const Text('Index 1: Business', style: optionStyle),
-              const Text('Index 2: School', style: optionStyle),
-            ][currentPageIndex],
-      ),
       bottomNavigationBar: Navbar(
         onItemTapped: _onItemTapped,
         currentPageIndex: currentPageIndex,
+      ),
+
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => UserDisplayCubit()..displayUser()),
+          BlocProvider(create: (context) => ButtonStateCubit()),
+          BlocProvider(create: (context) => AuthStateCubit()),
+        ],
+        child: BlocBuilder<UserDisplayCubit, UserDisplayState>(
+          builder: (context, state) {
+            if (state is UserLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (state is LoadUserFailure) {
+              return Center(child: Text(state.errorMessage));
+            }
+            if (state is UserLoaded) {
+              return [
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Consumer(
+                        builder:
+                            (context, ThemeNotifier themeNotifier, child) =>
+                                SwitchListTile(
+                                  title: Text("Dark Mode"),
+                                  onChanged: (val) {
+                                    themeNotifier.toggleTheme();
+                                  },
+                                  value:
+                                      themeNotifier.theme == 'dark'
+                                          ? true
+                                          : false,
+                                ),
+                      ),
+                      Consumer(
+                        builder:
+                            (context, ThemeNotifier themeNotifier, child) =>
+                                FloatingActionButton(
+                                  heroTag: 'appColorSchemeDynamic',
+                                  onPressed: () {
+                                    themeNotifier.toggleColorScheme('dynamic');
+                                  },
+                                  child: Icon(Icons.add),
+                                ),
+                      ),
+                      Consumer(
+                        builder:
+                            (context, ThemeNotifier themeNotifier, child) =>
+                                FloatingActionButton(
+                                  heroTag: 'appColorSchemeMain',
+                                  onPressed: () {
+                                    themeNotifier.toggleColorScheme('main');
+                                  },
+                                  child: Icon(Icons.add),
+                                ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        state.userEntity.email,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 19,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(32),
+                        child: Consumer(
+                          builder: (
+                            context,
+                            ThemeNotifier themeNotifier,
+                            child,
+                          ) {
+                            return BestyButton(
+                              title: 'Logout',
+                              onPressed: () {
+                                themeNotifier.toggleColorScheme('main');
+                                if (themeNotifier.theme == 'dark') {
+                                  themeNotifier.toggleTheme();
+                                }
+                                context.read<ButtonStateCubit>().execute(
+                                  usecase: SignoutUseCase(),
+                                );
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SigninPage(),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Text('Index 1: Business', style: optionStyle),
+                const Text('Index 2: School', style: optionStyle),
+              ][currentPageIndex];
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
